1e76f5bfc4ba9adfeb09a4fc7430b077294ff6a6
Maria <mariajoserivasmejia60@gmail.com>
2026-07-26 00:01:51 -0600

    ArregloLote

--- lote.js
+++ lote.js
@@ -4,13 +4,74 @@ module.exports = {
     table: "lote",
     id: "id_lote",
     fields: [
-        { name: "id_medicamento", label: "ID Medicamento", type: "number", min: 1, required: true },
-        { name: "numero_lote", label: "Número Lote", required: true },
-        { name: "cantidad_inicial", label: "Cantidad Inicial", type: "number", min: 0, required: true },
-        { name: "cantidad_disponible", label: "Cantidad Disponible", type: "number", min: 0, required: true },
-        { name: "fecha_fabricacion", label: "Fecha Fabricación", type: "date", required: true },
-        { name: "fecha_vencimiento", label: "Fecha Vencimiento", type: "date", required: true },
-        { name: "precio_compra", label: "Precio Compra", type: "number", min: 0, step: "0.01", required: true },
-        { name: "estado", label: "Estado", type: "select", options: ["Disponible", "Agotado", "Vencido", "Retirado"] }
+        {
+            name: "id_medicamento",
+            label: "Nombre Medicamento",
+            type: "medicine-name",
+            displayName: "nombre_medicamento",
+            showInTable: true,
+            required: true
+        },
+        {
+            name: "numero_lote",
+            label: "Número Lote",
+            readOnly: true,
+            required: true
+        },
+        {
+            name: "stock_total",
+            label: "Stock Total",
+            type: "number",
+            virtual: true,
+            showInTable: true,
+            readOnly: true
+        },
+        {
+            name: "laboratorio",
+            label: "Laboratorio",
+            virtual: true,
+            readOnly: true
+        },
+        {
+            name: "formas_venta",
+            label: "Formas de Venta y Precios",
+            virtual: true,
+            full: true
+        },
+        {
+            name: "cantidad_inicial",
+            label: "Cantidad a Ingresar",
+            type: "number",
+            min: 1,
+            required: true
+        },
+        {
+            name: "fecha_fabricacion",
+            label: "Fecha Fabricación",
+            type: "date",
+            required: true
+        },
+        {
+            name: "fecha_vencimiento",
+            label: "Fecha Vencimiento",
+            type: "date",
+            required: true
+        },
+        {
+            name: "precio_compra",
+            label: "Precio Compra",
+            type: "number",
+            min: 0,
+            step: "0.01",
+            required: true
+        },
+        {
+            name: "precio_compra_total",
+            label: "Precio de Compra Total",
+            type: "number",
+            virtual: true,
+            showInTable: true,
+            readOnly: true
+        }
     ]
 };
