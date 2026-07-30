033710ddc1a907330e4f20ba9d039d4771058420
Maria <mariajoserivasmejia60@gmail.com>
2026-07-30 15:04:43 -0600

    Ahora si ya casi

--- medicamentos.js
+++ medicamentos.js
@@ -6,7 +6,6 @@ module.exports = {
     fields: [
         { name: "codigo", label: "Código", readOnly: true },
         { name: "nombre", label: "Nombre", required: true },
-        { name: "descripcion", label: "Descripción", type: "textarea", full: true },
         { name: "categoria", label: "Categoría" },
         { name: "presentacion", label: "Presentación", wide: true },
         { name: "estado", label: "Estado", type: "select", options: ["Disponible", "Agotado", "Inactivo"] }
